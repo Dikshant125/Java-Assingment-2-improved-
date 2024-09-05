41b8cf8cbb47c0e51a44f922d68a44c01a5dde07
Dikshant125 <dikshantgoswami441@gmail.com>
2024-09-05 13:05:09 +0530

    Add files via upload
    this is a improved version of Assingment-2 Basic operations on String using Java

---
+++ Constants.java
@@ -0,0 +1,32 @@
+public class Constants {
+    public static final String ENTER_STRING = "Enter the string: ";
+    public static final String WORD_COUNT = "Word count: ";
+    public static final String NO_REPEATED_CHARACTERS = "No repeated characters found.";
+    public static final String MOST_REPEATED_CHARACTER = " -> ";
+    public static final String INVALID_INDEX = "Invalid index";
+    public static final String ENTER_OLD_SUBSTRING = "Enter the old substring: ";
+    public static final String ENTER_NEW_SUBSTRING = "Enter the new substring: ";
+    public static final String SORTED_STRING = "Sorted string: ";
+    public static final String SHIFTED_STRING = "Shifted string: ";
+    public static final String IS_PALINDROME = "Is palindrome: ";
+    public static final String PLEASE_ENTER_INTEGER = "Please enter an integer from 1 to 10 only";
+    public static final String INVALID_CHOICE = "Invalid choice! Please try again.";
+    public static final String EXIT_PROMPT = "Do you want to exit? (yes/no): ";
+    public static final String ENTER_START_INDEX = "Enter the start: ";
+    public static final String ENTER_END_INDEX = "Enter the end: ";
+    public static final String ENTER_REVERSE_STRING = "Enter the string to reverse:";
+    public static final String ENTER_SHIFT_AMOUNT = "Enter the number of digits you want to shift: ";
+    public static final String ENTER_STRING_OPERATION = "Enter a string operation:";
+    public static final String OPTION_APPEND = "1) Append a string";
+    public static final String OPTION_SPLIT = "2) Split a string";
+    public static final String OPTION_REPLACE = "3) Replace a string";
+    public static final String OPTION_SPLICE = "4) Splice";
+    public static final String OPTION_REVERSE = "5) Reverse a string";
+    public static final String OPTION_SHIFT = "6) Shift characters";
+    public static final String OPTION_SORT = "7) Sort characters";
+    public static final String OPTION_COUNT = "8) Count words";
+    public static final String OPTION_PALINDROME = "9) Check palindrome";
+    public static final String OPTION_MAX_FREQUENCY = "10) Max frequency character";
+    public static final String LENGTH_WARN  =   "The shift value is greater than the length";
+    public static final string FREQUENCY_GET = "Enter any word & get the most repeated character: "
+}
